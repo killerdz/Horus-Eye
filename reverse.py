af028cd063321e1e32ae49dc38b63d68ac332fd5
X-LINE <54470120+Djawed-Hammadi@users.noreply.github.com>
2020-05-24 02:00:27 +0200

    Add files via upload
    Simple Script To Get IP ADDRESS

---
+++ reverse.py
@@ -0,0 +1,7 @@
+#!/bin/python3 
+from time import sleep 
+from socket import gethostbyname
+host = input("\033[1;37mPlease Enter The Host : ")
+sleep(5)
+print("\n" + "Ip For",host," Is : \033[1;33m",gethostbyname(host))
+
