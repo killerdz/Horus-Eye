431a105d7c742323404682388c49f6b60d270aaf
Kira Dz <khaledtad9@gmail.com>
2020-05-31 18:20:13 +0100

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,35 @@
+#!usr/bin/python3 
+#!/bin/python3 
+#Banner For The Script : 
+#The Script Must Start With This Banner So Please Don't Delete This File :'( 
+import sys
+import os
+import time
+os.system('clear')
+print ('''
+\033[0;31m     .-----.                  \033[1;37m __ __   ___   ____  __ __  _____          ___  __ __    ___ 
+\033[0;31m   .' -   - '.                \033[1;37m|  |  | /   \ |    \|  |  |/ ___/         /  _]|  |  |  /  _]
+\033[0;31m  /  .-. .-.  \               \033[1;37m|  |  ||     ||  D  )  |  (   \_  _____  /  [_ |  |  | /  [_ 
+\033[0;31m  |  | | | |  |               \033[1;37m|  _  ||  O  ||    /|  |  |\__  ||     ||    _]|  ~  ||    _]
+\033[0;31m   \ \o/ \o/ /                \033[1;37m|  |  ||     ||    \|  :  |/  \ ||_____||   [_ |___, ||   [_ 
+\033[0;31m  _/    ^    \_               \033[1;37m|  |  ||     ||  .  \     |\    |       |     ||     ||     |
+\033[0;31m | \  '---'  / |              \033[1;37m|__|__| \___/ |__|\_|\__,_| \___|       |_____||____/ |_____|
+\033[0;31m / /`--. .--`\ \                                                                           \033[0;34mv2.0
+\033[0;31m/ /'---` `---'\ \                           \033[1;33mCoded By @\033[0;31mDjawed Hammadi (X-line)
+\033[0;31m'.__.       .__.'                            \033[1;33mDEveloped by@\033[0;31mkhalid (KIRA-DZ)
+\033[0;31m    `|     |`                                 \033[1;37mFACEBOOK:\033[0;32mDjawedHammadi
+\033[0;31m     |     \                                   \033[1;37mFACEBOOK:\033[0;32mkhalid Tadjeri
+\033[0;31m     \      '--.                                \033[1;37mGITHUB:\033[0;32mDjawed-Hammadi
+\033[0;31m      '.        `\                               \033[1;37mGITHUB:\033[0;32mKira Dz
+\033[0;31m        `'---.   |
+\033[0;31m           ,__) /
+\033[0;31m            `..'
+                                
+''')
+def Slowprint(banner):
+    for dido in banner + '\n' :
+        sys.stdout.write(dido)
+        sys.stdout.flush()
+        time.sleep(10. / 100)
+Slowprint("\033[0;32m[\033[1;37m#\033[0;32m] \033[1;37mStarting After [ \033[0;36m3 . 2 . 1 \033[1;37m] : \n")
+
